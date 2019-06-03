8b79a9f325f361abd9fa3b0b3bda5901e6764aec
lordgarmvd <50119608+lordgarmvd@users.noreply.github.com>
2019-06-03 11:10:27 -0400

    Add files via upload

---
+++ javascript/index3.js
@@ -0,0 +1,6 @@
+var img = document.createElement("img");
+ 
+img.src = "C:\Users\Johnny\Desktop\WEB FILES\staff-only\pictures\00.jpg";
+var src = document.getElementById("pic01");
+ 
+src.appendChild(img);
